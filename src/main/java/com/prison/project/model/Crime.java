6e9e5493f6aec6b24da271f27d090ece7aae06f0
Anzelika Gorbaca <anzelika.spila@gmail.com>
2021-08-03 20:09:02 +0300

    added service annotations to classes and validation dependencies created prisoner model added @Entity and Id to classes

--- src/main/java/com/prison/project/model/Crime.java
+++ src/main/java/com/prison/project/model/Crime.java
@@ -1,4 +1,12 @@
 package com.prison.project.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Crime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "crime_id")
+    private Long id;
 }
