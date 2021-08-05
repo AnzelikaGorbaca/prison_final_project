d06036accaa0f0d94c2129d73d718fcd1826ac82
Daciite <dace.bebre@gmail.com>
2021-08-05 17:06:21 +0300

    Unique constraint exception added to staffcontroller and staff-unique.html added_SECOND part

--- src/main/java/com/prison/project/service/staff/CreateStaffService.java
+++ src/main/java/com/prison/project/service/staff/CreateStaffService.java
@@ -1,11 +1,18 @@
 package com.prison.project.service.staff;
 
+import com.prison.project.exception.BadRequestException;
 import com.prison.project.model.Staff;
 import com.prison.project.repository.StaffRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+import javax.validation.constraints.NotBlank;
+import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
 
 
 @Transactional
@@ -15,8 +22,21 @@ public class CreateStaffService {
 
     private final StaffRepository staffRepository;
 
-    public Staff registerStaff(Staff staff) {
-        staffRepository.save(staff);
+  /*  @Autowired
+    private final GetStaffService getStaffService;*/
+
+    public Staff registerStaff(Staff staff)  {
+
+
+        /*List<Staff> staffList = getStaffService.findAllStaff();
+        for (Staff s : staffList) {
+            if (staff.getPersonalCode().contains(s.getPersonalCode())) {
+                throw new BadRequestException("Staff member with such personal code already exists");
+            }
+        }*/
+
+
+       staffRepository.save(staff);
         return staff;
     }
 }
