218027386db4179c069e34ce1cc4a6d52a689023
DaBarca88 <marija.dolotova@gmail.com>
2021-08-11 03:22:37 +0300

    added statistics, changed html

--- src/main/java/com/prison/project/model/Punishment.java
+++ src/main/java/com/prison/project/model/Punishment.java
@@ -4,6 +4,7 @@ package com.prison.project.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.Collection;
 
 import static javax.persistence.FetchType.EAGER;
@@ -17,7 +18,7 @@ public class Punishment {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "punishment_id")
     private Long id;
-    //    @NotBlank(message = "Imprisonment duration is required")
+    @NotNull(message = "Imprisonment duration is required")
     @Column(name="imprisonment_months")
     private Integer imprisonmentMonths;
 }
