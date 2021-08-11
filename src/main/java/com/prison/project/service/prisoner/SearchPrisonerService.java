2753ccbf4fca265346b00a04aea8b69d7aaedf3d
Anzelika Gorbaca <anzelika.spila@gmail.com>
2021-08-11 14:30:43 +0300

    fail to add search

--- src/main/java/com/prison/project/service/prisoner/SearchPrisonerService.java
+++ src/main/java/com/prison/project/service/prisoner/SearchPrisonerService.java
@@ -1,16 +1,18 @@
 package com.prison.project.service.prisoner;
 
-import com.prison.project.model.Prisoner;
-import com.prison.project.model.PrisonerSearch;
-import com.prison.project.model.Staff;
-import com.prison.project.model.StaffSearch;
+import com.prison.project.model.*;
 import com.prison.project.repository.PrisonerRepository;
+import com.prison.project.repository.PunishmentRepository;
+import com.prison.project.service.crime.GetCrimeService;
+import com.prison.project.service.punishment.GetPunishmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.data.domain.ExampleMatcher.matchingAll;
 
@@ -20,6 +22,8 @@ import static org.springframework.data.domain.ExampleMatcher.matchingAll;
 public class SearchPrisonerService {
 
     public final PrisonerRepository repository;
+    public final GetPunishmentService punishmentService;
+    public final GetCrimeService getCrimeService;
 
     public List<Prisoner> searchPrisoner(PrisonerSearch prisonerSearch) {
         Prisoner prisoner = new Prisoner();
@@ -28,8 +32,8 @@ public class SearchPrisonerService {
         prisoner.setSurname(prisonerSearch.getSurname());
         prisoner.setPersonalCode(prisonerSearch.getPersonalCode());
         prisoner.setAddress(prisonerSearch.getAddress());
-        prisoner.setCrimes(prisonerSearch.getCrimes());
-        prisoner.setPunishment(prisonerSearch.getPunishment());
+        prisoner.setCrimes(getCrimesJson(prisonerSearch.getCrimesJson()));
+        prisoner.setPunishment(getPunishmentByID(prisonerSearch.getPunishmentId()));
         prisoner.setStartDate(prisonerSearch.getEndDate());
         prisoner.setEndDate(prisonerSearch.getEndDate());
 
@@ -38,4 +42,18 @@ public class SearchPrisonerService {
         return repository.findAll(prisonerExample);
     }
 
+    private Punishment getPunishmentByID(Long id) {
+        if (id == null){
+            return null;
+        }
+        return punishmentService.getPunishmentById(id);
+    }
+
+    public List<Crime> getCrimesJson(String crimeJson) {
+        if (crimeJson == null) {
+            return null;
+        }
+        return getCrimeService.getCrimesJson(crimeJson);
+    }
 }
+
