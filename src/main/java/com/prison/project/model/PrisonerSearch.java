9a8d5dfe92fab06c156fc86dfa4e3e7562b9c465
Anzelika Gorbaca <anzelika.spila@gmail.com>
2021-08-16 19:01:21 +0300

    create Prisoner Test update

--- src/main/java/com/prison/project/model/PrisonerSearch.java
+++ src/main/java/com/prison/project/model/PrisonerSearch.java
@@ -2,6 +2,7 @@ package com.prison.project.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.format.annotation.DateTimeFormat;;
 import javax.persistence.*;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PrisonerSearch {
 
     private String name;
