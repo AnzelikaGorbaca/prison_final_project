bbfcdd1d28515724b5748b20684448961f611578
Anzelika Gorbaca <anzelika.spila@gmail.com>
2021-08-16 20:56:04 +0300

    changed prisoner photo service class

--- src/main/java/com/prison/project/service/prisoner/PhotoServicePrisoner.java
+++ src/main/java/com/prison/project/service/prisoner/PhotoServicePrisoner.java
@@ -10,7 +10,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.exceptions.TemplateInputException;
 
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.nio.file.*;
 import java.util.Objects;
 
 @Service
@@ -18,6 +19,8 @@ import java.util.Objects;
 public class PhotoServicePrisoner {
 
     private final UpdatePrisonerService updatePrisonerService;
+    private final CreatePrisonerService createPrisonerService;
+    private final GetPrisonerService getPrisonerService;
 
     public boolean checkPhotoForErrorsAndUpload(Long id, Prisoner prisoner, MultipartFile multipartFile) {
 
@@ -36,4 +39,55 @@ public class PhotoServicePrisoner {
         }
         return false;
     }
+
+    public void uploadPhotoRegister(Prisoner prisoner, MultipartFile multipartFile) {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        prisoner.setPhoto(fileName);
+        Prisoner savedPrisoner = createPrisonerService.registerPrisoner(prisoner);
+        String uploadDir = "photos/" + "prisoner_" + savedPrisoner.getId();
+        saveFile(uploadDir, fileName, multipartFile);
+    }
+
+    public void deletePhoto (Long id){
+        Path path = Paths.get("photos/" + "prisoner_" + id + "/" + getPrisonerService.getPrisonerById(id).getPhoto());
+        FileUploadUtil.deleteFile(path);
+        Path dir = Paths.get("photos/" + "prisoner_" + id);
+        deleteFile(dir);
+    }
+
+
+    private void saveFile(String uploadDir, String fileName,
+                          MultipartFile multipartFile) {
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            try {
+                Files.createDirectories(uploadPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new RuntimeException("Could not save image file: " + fileName, ioe);
+        }
+
+    }
+
+    private void deleteFile(Path path) {
+
+        try {
+            Files.delete(path);
+        } catch (NoSuchFileException ex) {
+            System.out.printf("No such file or directory: %s\n", path);
+        } catch (DirectoryNotEmptyException ex) {
+            System.out.printf("Directory %s is not empty\n", path);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
 }
+
+
