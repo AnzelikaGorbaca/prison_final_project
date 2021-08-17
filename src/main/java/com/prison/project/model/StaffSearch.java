f8eb8f1b824f16aadbfd8835418ed21105c5421f
Daciite <dace.bebre@gmail.com>
2021-08-17 10:15:20 +0300

    SearchStaffTests added

--- src/main/java/com/prison/project/model/StaffSearch.java
+++ src/main/java/com/prison/project/model/StaffSearch.java
@@ -1,9 +1,13 @@
 package com.prison.project.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StaffSearch {
 
     private String name;
