aa5ceec1f3ec68557ec59a618729651f67badb68
Daciite <dace.bebre@gmail.com>
2021-08-17 10:37:55 +0300

    Addedtest StaffSearchTest for if nothing matches

--- src/test/java/com/prison/project/service/staff/SearchStaffServiceTests.java
+++ src/test/java/com/prison/project/service/staff/SearchStaffServiceTests.java
@@ -58,4 +58,49 @@ public class SearchStaffServiceTests {
         verify(staffRepository).findAll(isA(Example.class));
     }
 
+    @Test
+    void shouldReturnAllStaffIfNothingMatchesSearch(){
+        List<Staff> sampleList = Arrays.asList(new Staff(1L,"Elvis","Presley","GUARD","123456-12345",
+                        "+371212345678","AddressConsistingOf10","Moranto.jpg"),
+                new Staff(2L,"John","Walker","ACCOUNTANT","123456-00000",
+                        "+371212345677","AddressConsistingOf11","Moranto1.jpg"));
+        StaffSearch staffSearch = new StaffSearch("Elvis1","Presley1","GUARD1","123456-12341",
+                "+371212345671","AddressConsistingOf11");
+
+        when(staffRepository.findAll(isA(Example.class))).thenReturn(sampleList);
+
+        List<Staff> foundStaff = searchStaffService.searchStaff(staffSearch);
+
+        assertEquals(sampleList.size(),foundStaff.size());
+
+
+        verify(staffRepository).findAll(isA(Example.class));
+    }
+
+    @Test
+    void shouldReturnSearchedStaffIfOnlyOneSearchCriteriaMatches(){
+        List<Staff> sampleList = Arrays.asList(new Staff(1L,"Elvis","Presley","GUARD","123456-12345",
+                        "+371212345678","AddressConsistingOf10","Moranto.jpg"),
+                new Staff(2L,"John","Walker","ACCOUNTANT","123456-00000",
+                        "+371212345677","AddressConsistingOf11","Moranto1.jpg"));
+        StaffSearch staffSearch = new StaffSearch("Elvis","Presley1","GUARD1","123456-12341",
+                "+371212345671","AddressConsistingOf11");
+
+        when(staffRepository.findAll(isA(Example.class))).thenReturn(sampleList);
+
+        List<Staff> foundStaff = searchStaffService.searchStaff(staffSearch);
+
+        assertEquals(staffSearch.getName(),foundStaff.get(0).getName());
+        assertNotEquals(staffSearch.getName(), foundStaff.get(1).getName());
+
+        assertNotEquals(staffSearch.getSurname(), foundStaff.get(0).getSurname());
+        assertNotEquals(staffSearch.getAddress(),foundStaff.get(0).getAddress());
+        assertNotEquals(staffSearch.getOccupation(),foundStaff.get(0).getOccupation());
+        assertNotEquals(staffSearch.getPhoneNumber(),foundStaff.get(0).getPhoneNumber());
+        assertNotEquals(staffSearch.getPersonalCode(),foundStaff.get(0).getPersonalCode());
+
+
+        verify(staffRepository).findAll(isA(Example.class));
+    }
+
 }
