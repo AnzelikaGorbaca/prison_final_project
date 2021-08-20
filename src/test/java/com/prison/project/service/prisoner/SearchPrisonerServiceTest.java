6005953c3add1d445b73dd6e03621f3de5b26e28
Anzelika Gorbaca <anzelika.spila@gmail.com>
2021-08-20 21:30:28 +0300

    photo

--- src/test/java/com/prison/project/service/prisoner/SearchPrisonerServiceTest.java
+++ src/test/java/com/prison/project/service/prisoner/SearchPrisonerServiceTest.java
@@ -41,8 +41,7 @@ class SearchPrisonerServiceTest {
     GetPrisonerService getPrisonerService;
     @Mock
     StatusPrisonerService statusPrisonerService;
-//    @Mock
-//    PrisonerSearch prisonerSearch;
+
 
 
     private LocalDate getStartDate() {
